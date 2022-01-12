2b0b137f3db5d92dea256fb4b50330531c8f4d0d
lihiopp <93098326+lihiopp@users.noreply.github.com>
2022-01-12 18:17:14 +0200

    version 2
    Thread used instead of process.
    Not allowing the files to be downloaded (0 KB)

--- Client.py
+++ Client.py
@@ -1,17 +1,26 @@
-import socket, subprocess
+import socket, os
+#import subprocess, sys
+import threading
+import NotifyChanges as nc
+
 
 class Client:
     def __init__(self,ip,port):
         self.socket = socket.socket()
         self.socket.connect((ip,port))
-        print("Initialized and connected.")
+        self.files=[]
+        self.thread = threading.Thread(target=nc.main, args=(self.files,)).start()##
+        print("Initialized, connected to server. Starts monitoring.")
 
     def handle_file(self):
         '''sends downloaded files to the server and gets scan results.'''
-        code = subprocess.run(['python','NotifyChanges.py'],shell=True)
-        filename = subprocess.check_output(['python','NotifyChanges.py'],shell=True)
-        self.socket.send(filename)
-        with open(filename.decode(),'rb') as f:
+        while True:
+            if(len(self.file)>0):
+                filename = self.files.pop(0)
+                break
+                
+        self.socket.send(filename.encode())
+        with open(filename,'rb') as f:
             while True:
                 data = f.read(1024)
                 if not data:
@@ -19,8 +28,9 @@ class Client:
                     break
                 self.socket.send(data)
 
-        report = self.socket.recv(1024).decode()
-        print(report) #to do later - save results to a file
+        report = self.socket.recv(1024).decode()#it is stuck here
+        with open("answer.txt",'w') as f:
+            f.write(filename + "--->" + report + "\r\n")
 
 
 def main():
